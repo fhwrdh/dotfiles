81ca3132cc05baad20e4d44db79bde8b0670969c
Franklin Henderson <fhwrdh@fhwrdh.net>
2019-12-08 22:36:35 +0000

    turn off history customization

--- custom/03_history.zsh
+++ custom/03_history.zsh
@@ -1,9 +1,14 @@
-setopt SHARE_HISTORY                  # share history across multiple zsh sessions
-setopt APPEND_HISTORY                 # append to history
-setopt INC_APPEND_HISTORY             # adds commands as they are typed, not at shell exit
-setopt HIST_IGNORE_DUPS               # do not store duplications
-setopt HIST_REDUCE_BLANKS             # removes blank lines from history
-HISTFILE=$HOME/.zsh_history
-HISTCONTROL=ignoreboth
-HISTSIZE=2000
-SAVEHIST=2000
+# echo 'history'
+
+# setopt SHARE_HISTORY                  # share history across multiple zsh sessions
+# setopt APPEND_HISTORY                 # append to history
+# setopt INC_APPEND_HISTORY             # adds commands as they are typed, not at shell exit
+# setopt HIST_IGNORE_DUPS               # do not store duplications
+# setopt HIST_REDUCE_BLANKS             # removes blank lines from history
+# HISTFILE=$HOME/.zsh_history
+# HISTCONTROL=ignoreboth
+# HISTSIZE=2000
+# SAVEHIST=2000
+
+# setopt
+# cat  $HOME/.zsh_history
